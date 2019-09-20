d61951444830cca60c64e53de63897dfb1ee86fb
VinhKietLa <47901573+VinhKietLa@users.noreply.github.com>
2019-09-20 14:30:07 +0100

    Mobile styles complete

--- index.js
+++ index.js
@@ -9,20 +9,20 @@ const w3 = document.querySelector('.w3');
 //Function that converts user input into selected metric output for weight
 const weightConversion = () => {
     let userInput = userWeightInput.value;
-    if(selectMetric.value === 'KG'){
+    if(selectWeight.value === 'KG'){
       w1.innerHTML = '<p>POUNDS: ' + (userInput * 2.205).toFixed(2) + '</p>';
       w2.innerHTML = '<p>STONE: ' + (userInput / 6.35).toFixed(2) + '</p>';
       w3.innerHTML = '<p>GRAMS: ' + userInput * 1000 + '</p>';
     }
-    else if (selectMetric.value === 'POUNDS'){
+    else if (selectWeight.value === 'POUNDS'){
         w1.innerHTML = '<p>KILOGRAM: ' + (userInput / 2.205).toFixed(2) + '</p>';
         w2.innerHTML = '<p>STONE: ' + (userInput / 14).toFixed(2) + '</p>';
         w3.innerHTML = '<p>GRAMS: ' + userInput * 453.592 + '</p>';
-    } else if (selectMetric.value === 'STONES'){
+    } else if (selectWeight.value === 'STONES'){
         w1.innerHTML = '<p>KILOGRAM: ' + (userInput * 6.35).toFixed(2) + '</p>';
         w2.innerHTML = '<p>POUNDS: ' + (userInput * 14).toFixed(2) + '</p>';
         w3.innerHTML = '<p>GRAMS: ' + (userInput * 6350.293).toFixed(2) + '</p>';
-    } else if(selectMetric.value === 'GRAMS'){
+    } else if(selectWeight.value === 'GRAMS'){
         w1.innerHTML = '<p>KILOGRAM: ' + (userInput / 1000).toFixed(2) + '</p>';
         w2.innerHTML = '<p>POUNDS: ' + (userInput / 453.592).toFixed(2) + '</p>';
         w3.innerHTML = '<p>STONES: ' + (userInput / 6350.293).toFixed(3) + '</p>';
